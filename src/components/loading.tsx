6080453fe77b0fbefae7f8df467ea89bdea67559
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-28 21:01:30 -0300

    update loading style

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,24 +3,32 @@ import Image from 'next/image';
 import { motion } from 'motion/react';
 
 export function Loading() {
+    const dotVariants = {
+        pulse: {
+            scale: [1, 1.5, 1],
+            transition: {
+                duration: 1.2,
+                repeat: Infinity,
+                ease: 'easeInOut',
+            },
+        },
+    }
+
     return (
         <div className='absolute top-0 left-0 w-screen h-screen bg-background flex flex-col items-center justify-center gap-20 py-20'>
-            <div className='text-lg text-center uppercase flex gap-1.5'>
+            <div className='text-lg text-center uppercase flex gap-2 items-baseline'>
                 <span>Loading</span>
 
-                <motion.span
-                    animate={{ opacity: [0.2, 1, 0.2], x: [0, 0, 0] }}
-                    transition={{
-                        duration: 1.5,
-                        repeat: Infinity,
-                        ease: 'linear'
-                    }}
-                    style={{
-                        letterSpacing: '4px'
-                    }}
+                <motion.div
+                    animate='pulse'
+                    transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
+                    className='container'
                 >
-                    ...
-                </motion.span>
+                    <motion.div className='dot bg-foreground' variants={dotVariants} />
+                    <motion.div className='dot bg-foreground' variants={dotVariants} />
+                    <motion.div className='dot bg-foreground' variants={dotVariants} />
+                    <StyleSheet />
+                </motion.div>
             </div>
 
             <div className='w-50'>
@@ -34,5 +42,27 @@ export function Loading() {
                 />
             </div>
         </div>
+    )
+}
+
+function StyleSheet() {
+    return (
+        <style>
+            {`
+            .container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 8px;
+            }
+
+            .dot {
+                width: 2px;
+                height: 2px;
+                border-radius: 50%;
+                will-change: transform;
+            }
+            `}
+        </style>
     )
 }
