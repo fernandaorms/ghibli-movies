ee8b2e0e2b44de45b677031bf704a2d9b459f7e1
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-29 07:25:17 -0300

    rm - fixed hero bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
                 <section
                     id='hero'
                     className='relative pt-32 h-[100vh]'>
-                    <div className='lg:block hidden background-image !bg-fixed' style={{backgroundImage: 'url(/hero-bg-lg.webp)'}}></div>
-                    <div className='hidden max-lg:block  background-image !bg-fixed' style={{backgroundImage: 'url(/hero-bg-md.webp)'}}></div>
+                    <div className='lg:block hidden background-image' style={{backgroundImage: 'url(/hero-bg-lg.webp)'}}></div>
+                    <div className='hidden max-lg:block  background-image' style={{backgroundImage: 'url(/hero-bg-md.webp)'}}></div>
                     <div className='relative wrapper flex items-center justify-center h-full'>
                             <h1 className='xl:text-7xl text-white'>Hero title bla bla bla</h1>
                     </div>
