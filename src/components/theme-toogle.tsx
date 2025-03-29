48cd90694e9f2506159b890297b719da17b7a4c4
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-29 13:57:23 -0300

    header - implement mobile menu

--- src/components/theme-toogle.tsx
+++ src/components/theme-toogle.tsx
@@ -6,7 +6,10 @@ import { AnimatePresence, motion } from 'motion/react';
 
 import { FaMoon, FaSun } from "react-icons/fa6";
 
-export function ThemeToggle() {
+type Props = {
+    isOpen: boolean
+}
+export function ThemeToggle(props: Props) {
     const { theme, setTheme } = useTheme();
 
     if (!theme) return <div className='bg-transparent h-10 w-10 rounded-full'></div>;
@@ -14,7 +17,7 @@ export function ThemeToggle() {
     return (
         <>
             <motion.div
-                className='bg-foreground-light h-10 w-10 flex items-center justify-center rounded-full cursor-pointer'
+                className={`bg-foreground-light h-10 w-10 flex items-center justify-center rounded-full cursor-pointer z-10${props.isOpen ? ' text-white' : ''}`}
                 onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
                 whileHover={{ scale: 1.125 }}
             >
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -4,22 +4,34 @@ import Link from 'next/link';
 import { ThemeToggle } from '@/components/theme-toogle';
 import { Logo } from '@/components/logo';
 import { usePathname } from 'next/navigation';
-import { motion, useMotionValueEvent, useScroll } from 'motion/react';
-import { useState } from 'react';
+import { AnimatePresence, motion, useMotionValueEvent, useScroll } from 'motion/react';
+import { useEffect, useState } from 'react';
+import { FaMagnifyingGlass, FaAlignJustify, FaXmark, FaX } from 'react-icons/fa6';
 
 export function Header() {
     const pathname = usePathname();
-    const [scrolled, setScrolled] = useState(false);
     const { scrollY } = useScroll();
+    const [scrolled, setScrolled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     useMotionValueEvent(scrollY, 'change', (latest) => {
         setScrolled(latest > 100);
     });
 
+    const toggleSearch = () => {
+        if (isOpen) setIsOpen(false);
+        console.log('Search toggle :)')
+    }
+
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+        console.log(isOpen);
+    }
+
     return (
         <>
             <motion.header
-                className='z-50 fixed w-full top-0 py-2 xl:py-4'
+                className='z-50 fixed w-full top-0 '
                 initial={{
                     color: 'var(--white)',
                     backgroundColor: 'rgba(0, 0, 0, 0)',
@@ -32,26 +44,75 @@ export function Header() {
                 }}
                 transition={{ duration: .3, ease: 'easeInOut' }}
             >
-                <div className='h-full w-full wrapper flex justify-between items-center'>
-                    <Logo />
+                <div className='h-full w-full'>
+                    <div className='h-full w-full wrapper flex justify-between items-center'>
+                        <Logo />
 
-                    <nav className='hidden xl:flex gap-8 text-sm font-semibold uppercase'>
-                        <Link href={'/'}>Home</Link>
-                        <Link href={'/movies'}>Movies</Link>
+                        <nav className='hidden xl:flex gap-8 text-sm font-semibold uppercase'>
+                            <Link href={'/'}>Home</Link>
+                            <Link href={'/movies'}>Movies</Link>
 
-                        {pathname === '/' && (
-                            <>
-                                <Link href={'#about'}>About</Link>
-                                <Link href={'#start'}>Start</Link>
-                                <Link href={'#tech'}>Tech</Link>
-                            </>
-                        )}
-                    </nav>
-                    
-                    <ThemeToggle />
+                            {pathname === '/' && (
+                                <>
+                                    <Link href={'#about'}>About</Link>
+                                    <Link href={'#start'}>Start</Link>
+                                    <Link href={'#tech'}>Tech</Link>
+                                </>
+                            )}
+                        </nav>
+
+                        <div className='flex justify-end gap-4'>
+                            <ThemeToggle isOpen={isOpen} />
+
+                            <motion.div
+                                className={`bg-foreground-light h-10 w-10 flex items-center justify-center rounded-full cursor-pointer z-10${isOpen ? ' text-white' : ''}`}
+                                onClick={toggleSearch}
+                                whileHover={{ scale: 1.125 }}
+                            >
+                                <FaMagnifyingGlass />
+                            </motion.div>
+
+                            <motion.div
+                                className={`xl:hidden bg-foreground-light h-10 w-10 flex items-center justify-center rounded-full cursor-pointer z-10${isOpen ? ' text-white' : ''}`}
+                                onClick={toggleOpen}
+                                whileHover={{ scale: 1.125 }}
+                            >
+                                {isOpen ? (
+                                    <FaXmark />
+                                ) : (
+                                    <FaAlignJustify />
+                                )}
+                            </motion.div>
+
+
+
+                        </div>
+                    </div>
+
+                    <AnimatePresence initial={false}>
+                        {isOpen ? (
+                            <motion.div
+                                className='xl:hidden bg-primary-md text-white fixed right-0 top-0 w-full max-w-[576px]'
+                                initial={{ opacity: 0, x: 50, paddingTop: 'calc(80px + 4rem)' }}
+                                animate={{ opacity: 1, x: 0 }}
+                                exit={{ opacity: 0, x: 50 }}
+                            >
+                                <nav className='h-screen flex flex-col gap-8 px-12 text-2xl uppercase'>
+                                    <Link href={'/'}>Home</Link>
+                                    <Link href={'/movies'}>Movies</Link>
+
+                                    {pathname === '/' && (
+                                        <>
+                                            <Link href={'#about'}>About</Link>
+                                            <Link href={'#start'}>Start</Link>
+                                            <Link href={'#tech'}>Tech</Link>
+                                        </>
+                                    )}
+                                </nav>
+                            </motion.div>
+                        ) : null}
+                    </AnimatePresence>
                 </div>
-
-
             </motion.header>
         </>
 
