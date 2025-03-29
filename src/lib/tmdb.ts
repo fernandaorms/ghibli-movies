cc6f5026d9771b97fd49aa29aeac0205ed853957
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-29 06:55:41 -0300

    add get-images function

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -1,25 +1,50 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const COMPANY_ID = process.env.NEXT_PUBLIC_COMPANY_ID;
+const IMG_BASE_URL = process.env.NEXT_PUBLIC_IMG_BASE_URL;
 
-async function fetchFromTMDB(endpoint: string, query?: string[]) {
-    let url = `${BASE_URL}${endpoint}?api_key=${API_KEY}`;
+async function fetchFromTMDB(endpoins: string, query?: string[]) {
+    let url = `${BASE_URL}/${endpoins}?api_key=${API_KEY}`;
     query?.forEach((value) => url += `&${value}`);
 
     const res = await fetch(url);
 
-    if (!res.ok) throw new Error(`Error fetching ${endpoint}`);
+    if (!res.ok) throw new Error(`Error fetching ${endpoins}`);
     return res.json();
 }
 
 export async function getAllMovies() {
-    return fetchFromTMDB(`/company/${COMPANY_ID}/movies`);
+    return fetchFromTMDB(`company/${COMPANY_ID}/movies`);
 }
 
 export async function getMovieByID(id: any) {
-    return fetchFromTMDB(`/movie/${id}`);
+    return fetchFromTMDB(`movie/${id}`);
 }
 
 export async function getMovieImages(id: any) {
-    return fetchFromTMDB(`/movie/${id}/images`, ['include_image_language=en,null']);
+    return fetchFromTMDB(`movie/${id}/images`, ['include_image_language=en,null']);
+}
+
+
+// Getting Images
+function getImage(file_path: string, file_size: string) {
+    return `${IMG_BASE_URL}${file_size}${file_path}`;
+}
+
+type BackdropSizes = 'w300' | 'w780' | 'w1280' | 'original';
+
+export function getBackdrop(file_path: string, file_size: BackdropSizes = 'original') {
+    return getImage(file_path, file_size);
+}
+
+type LogoSizes = 'w45' | 'w92' | 'w154' | 'w185' | 'w300' | 'w500' | 'original';
+
+export function getLogo(file_path: string, file_size: LogoSizes = 'original') {
+    return getImage(file_path, file_size);
+}
+
+type PosterSizes = 'w92' | 'w154' | 'w185' | 'w342' | 'w500' | 'w780' | 'original';
+
+export function getPoster(file_path: string, file_size: PosterSizes = 'original') {
+    return getImage(file_path, file_size);
 }
