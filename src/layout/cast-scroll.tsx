23d8510978e4b43b7f70eed4b7cef22ddb0666d6
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-30 15:25:12 -0300

    move + backdrop

--- src/layout/cast-scroll.tsx
+++ src/layout/cast-scroll.tsx
@@ -39,15 +39,15 @@ export function CastScroll(props: Props) {
                                 initial={{ scale: 1 }}
                                 whileHover={{ scale: 1.25 }}
                                 style={{ backgroundImage: `url(${getBackdropImage(cast.profile_path)})` }}
-                                className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-50'
+                                className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-10'
                             >
                                 <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-full transition-opacity'></div>
                             </motion.div>
 
                             <div className='text-center'>
                                 <span className='block font-semibold mt-3'>{cast.name}</span>
-                                <span className='block text-xs font-light mb-1'>({cast.original_name})</span>
-                                <span className='block text-sm'>{cast.character}</span>
+                                <span className='block text-xs font-light mb-1 text-content'>({cast.original_name})</span>
+                                <span className='block text-sm text-content'>{cast.character}</span>
                             </div>
                         </li>
                     ))}
