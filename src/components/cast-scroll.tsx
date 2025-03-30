6aa52c2340b1d30324702b726fc2432a9f5326cd
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-30 14:35:38 -0300

    update cast/crew scroll

--- src/components/cast-scroll.tsx
+++ src/components/cast-scroll.tsx
@@ -22,30 +22,36 @@ export function CastScroll(props: Props) {
                     <FaAnglesRight className='transition-transform' />
                 </Link>
             </div>
+            <div className='overflow-clip relative'>
+                <div className='absolute top-0 left-0 h-full w-10 bg-linear-to-r from-background to-transparent z-10'></div>
+                <div className='absolute top-0 right-0 h-full w-10 bg-linear-to-l from-background to-transparent z-10'></div>
 
-            <ol className='flex overflow-x-scroll py-5'>
-                {props.castArray.map((cast: any, index: number) => (
-                    <li
-                        key={cast.id}
-                        className='block !w-36 shrink-0'
-                    >
-                        <motion.div
-                            initial={{ scale: 1 }}
-                            whileHover={{ scale: 1.25 }}
-                            style={{ backgroundImage: `url(${getBackdropImage(cast.profile_path)})` }}
-                            className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-50'
-                        >
-                            <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-full transition-opacity'></div>
-                        </motion.div>
+                <motion.ol
+                    className='flex py-4'
+                    drag='x'
+                    dragConstraints={{ left: -250, right: 20 }}
+                    whileTap={{ cursor: 'grabbing' }}
+                >
+                    {props.castArray.map((cast: any) => (
+                        <li key={cast.id} className='block !w-36 shrink-0'>
+                            <motion.div
+                                initial={{ scale: 1 }}
+                                whileHover={{ scale: 1.25 }}
+                                style={{ backgroundImage: `url(${getBackdropImage(cast.profile_path)})` }}
+                                className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-50'
+                            >
+                                <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-full transition-opacity'></div>
+                            </motion.div>
 
-                        <div className='text-center'>
-                            <span className='block font-semibold mt-3'>{cast.name}</span>
-                            <span className='block text-xs font-light mb-1'>({cast.original_name})</span>
-                            <span className='block text-sm'>{cast.character}</span>
-                        </div>
-                    </li>
-                ))}
-            </ol>
+                            <div className='text-center'>
+                                <span className='block font-semibold mt-3'>{cast.name}</span>
+                                <span className='block text-xs font-light mb-1'>({cast.original_name})</span>
+                                <span className='block text-sm'>{cast.character}</span>
+                            </div>
+                        </li>
+                    ))}
+                </motion.ol>
+            </div>
         </div>
     )
 }
--- src/components/crew-scroll.tsx
+++ src/components/crew-scroll.tsx
@@ -23,29 +23,36 @@ export function CrewScroll(props: Props) {
                 </Link>
             </div>
 
-            <ol className='flex overflow-x-scroll py-4'>
-                {props.crewArray.map((crew: any, index: number) => (
-                    <li
-                        key={crew.id}
-                        className='block !w-36 shrink-0'
-                    >
-                        <motion.div
-                            initial={{ scale: 1 }}
-                            whileHover={{ scale: 1.25 }}
-                            style={{ backgroundImage: `url(${getBackdropImage(crew.profile_path)})` }}
-                            className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-50'
-                        >
-                            <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-full transition-opacity'></div>
-                        </motion.div>
+            <div className='overflow-clip relative'>
+                <div className='absolute top-0 left-0 h-full w-10 bg-linear-to-r from-background to-transparent z-10'></div>
+                <div className='absolute top-0 right-0 h-full w-10 bg-linear-to-l from-background to-transparent z-10'></div>
 
-                        <div className='text-center'>
-                            <span className='block font-semibold mt-3'>{crew.name}</span>
-                            <span className='block text-xs font-light mb-1'>({crew.original_name})</span>
-                            <span className='block text-sm'>{crew.job}</span>
-                        </div>
-                    </li>
-                ))}
-            </ol>
+                <motion.ol
+                    className='flex py-4'
+                    drag='x'
+                    dragConstraints={{ left: -250, right: 20 }}
+                    whileTap={{ cursor: 'grabbing' }}
+                >
+                    {props.crewArray.map((crew: any) => (
+                        <li key={crew.id} className='block !w-36 shrink-0'>
+                            <motion.div
+                                initial={{ scale: 1 }}
+                                whileHover={{ scale: 1.25 }}
+                                style={{ backgroundImage: `url(${getBackdropImage(crew.profile_path)})` }}
+                                className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-50'
+                            >
+                                <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-full transition-opacity'></div>
+                            </motion.div>
+
+                            <div className='text-center'>
+                                <span className='block font-semibold mt-3'>{crew.name}</span>
+                                <span className='block text-xs font-light mb-1'>({crew.original_name})</span>
+                                <span className='block text-sm'>{crew.job}</span>
+                            </div>
+                        </li>
+                    ))}
+                </motion.ol>
+            </div>
         </div>
     )
 }
