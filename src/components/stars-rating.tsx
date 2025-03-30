1273c963006e725dc4b92f5cc0e7c29074e583f4
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-30 15:41:36 -0300

    stars-rating - add optional color

--- src/components/stars-rating.tsx
+++ src/components/stars-rating.tsx
@@ -3,14 +3,16 @@ import { FaRegStar, FaStar } from 'react-icons/fa6';
 type Props = {
     ratingValue: number,
     starsNumber?: number,
+    color?: string,
 }
 
 export function StarsRating(props: Props) {
     const stars = props.starsNumber ?? 5;
+    const color = props.color ?? 'primary';
     const fill = Math.floor(props.ratingValue);
 
     return (
-        <div className='flex text-primary'>
+        <div className={`flex text-${color}`}>
             {[...Array(stars)].map((_, index) => (
                 <div key={index}>
                     {index + 1 <= fill ? (
