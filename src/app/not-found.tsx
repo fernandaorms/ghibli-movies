33658024685a552b86e2588ff0c3b9f0270f6dae
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-30 10:17:26 -0300

    update 404 layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,15 +10,41 @@ export default function NotFound() {
             <main>
                 <section
                     id='hero'
-                    className='relative pt-32 h-[100vh]'>
-                    <div className='lg:block hidden background-image' style={{ backgroundImage: 'url(/hero-bg-lg.webp)' }}></div>
-                    <div className='hidden max-lg:block  background-image' style={{ backgroundImage: 'url(/hero-bg-md.webp)' }}></div>
+                    className='relative pt-32 h-[40vh] xl:h-[50vh]'>
+                    <div className='lg:block hidden background-image bg-fixed' style={{ backgroundImage: 'url(/hero-bg-lg.webp)' }}></div>
+                    <div className='hidden max-lg:block  background-image bg-fixed' style={{ backgroundImage: 'url(/hero-bg-md.webp)' }}></div>
 
                     <div className='relative wrapper h-full text-center flex items-center justify-center'>
-                        <div className='w-full bg-amber grid gap-2'>
-                            <h2 className='text-2xl xl:text-4xl uppercase font-bold text-white'>Not Found</h2>
-                            <p>Could not find requested resource</p>
-                            <Link href='/' className='block mt-5'>Return Home</Link>
+                        <h1 className='text-2xl xl:text-4xl uppercase font-bold text-white pb-10'>404</h1>
+                    </div>
+                </section>
+
+                <section>
+                    <div className='wrapper py-16'>
+                        <div className='w-fit mx-auto text-foreground'>
+                            <div className='text-center mb-5'>
+                                <p className='text-2xl font-medium'>Oops! Page not found.</p>
+
+                                <p className='text-lg'>
+                                    The page you're looking for doesn't exist or may have been moved. {' '}
+                                </p>
+                            </div>
+
+                            <div className='mt-10 flex items-center justify-center gap-4'>
+                                <Link
+                                    href={'/movies'}
+                                    className='flex h-[48px] items-center bg-primary hover:bg-primary-md transition-colors text-white px-5 rounded-full cursor-pointer'
+                                >
+                                    Movies
+                                </Link>
+
+                                <Link
+                                    href={'/movies'}
+                                    className='flex h-[48px] items-center bg-transparent text-primary-md hover:text-primary border-2 border-primary-md hover:border-primary transition-colors px-5 rounded-full cursor-pointer'
+                                >
+                                    Homepage
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </section>
