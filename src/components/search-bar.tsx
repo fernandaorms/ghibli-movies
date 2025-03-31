888835b67fdb0331e4bdd81a41491d86c8cc6670
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-31 18:24:04 -0300

    search-bar - props destructuring

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -10,18 +10,18 @@ type Props = {
     clearSearchBar: () => void,
 }
 
-export function SearchBar(props: Props) {
+export function SearchBar({inputValue, scrolled,  isSearchOpen, onSubmit, onChange, clearSearchBar}: Props) {
     return (
         <AnimatePresence initial={false}>
-            {props.isSearchOpen ? (
+            {isSearchOpen ? (
                 <motion.div
-                    className={`wrapper ${props.scrolled ? 'mt-4' : ''}`}
+                    className={`wrapper ${scrolled ? 'mt-4' : ''}`}
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
                     exit={{ opacity: 0, y: 50 }}
                 >
                     <form
-                        onSubmit={(e) => props.onSubmit(e)}
+                        onSubmit={(e) => onSubmit(e)}
                         className='relative grid items-center gap-4 bg-background text-foreground rounded-full h-[56px]'
                     >
                         <div className='absolute left-4 w-[16px] pr-6 border-r-2 border-foreground-light'>
@@ -31,15 +31,15 @@ export function SearchBar(props: Props) {
                         <input
                             type='text'
                             placeholder='Serach at Ghibli Movies...'
-                            value={props.inputValue}
-                            onChange={(e) => props.onChange(e)}
+                            value={inputValue}
+                            onChange={(e) => onChange(e)}
                             className='w-full h-[100%] block rounded-full pl-13 pr-8 border-2 border-foreground-light focus:border-primary focus:outline-none'
                         />
 
                         <motion.div
                             className='absolute right-2 xl:right-3 xl:px-4 xl:text-sm flex gap-2 items-center justify-center cursor-pointer bg-foreground-light h-[40px] w-fit min-w-[40px] rounded-full'
                             whileHover={{ scale: 1.1 }}
-                            onClick={props.clearSearchBar}
+                            onClick={clearSearchBar}
                         >
                             <span className='hidden xl:inline-block uppercase'>Clear</span>
                             <FaArrowRotateLeft />
