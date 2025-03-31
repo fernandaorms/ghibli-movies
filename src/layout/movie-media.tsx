f6d64e3c206bbd45e8e9a50ffb1bf100c5b11934
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-31 09:47:58 -0300

    movie-media - refactoring

--- src/layout/movie-media.tsx
+++ src/layout/movie-media.tsx
@@ -9,11 +9,17 @@ type Props = {
     movieImages: any,
 }
 
-export function MovieMedia(props: Props) {
-    const backdrops = props.movieImages.backdrops ?? null;
-    const posters = props.movieImages.posters ?? null;
+type Media = {
+    id: number,
+    label: string,
+    images: any,
+}
 
-    const media = [
+export function MovieMedia({ movieImages }: Props) {
+    const backdrops = movieImages.backdrops ?? null;
+    const posters = movieImages.posters ?? null;
+
+    const media: Media[] = [
         { id: 0, label: 'Backdrops', images: backdrops },
         { id: 1, label: 'Posters', images: posters }
     ];
@@ -37,31 +43,11 @@ export function MovieMedia(props: Props) {
                 <nav className='flex gap-10 md:gap-16 items-baseline pb-2'>
                     <h2 className='font-medium text-xl md:text-2xl'>Media</h2>
 
-                    <ul className='flex gap-6 relative'>
-                        {media.map((item) => (
-                            <motion.li
-                                key={item.id}
-                                initial={false}
-                                whileHover={{
-                                    color: item.id === selectedTab.id ? 'var(--foreground)' : 'var(--primary)',
-                                    cursor: item.id === selectedTab.id ? 'default' : 'pointer'
-                                }}
-                                className='relative block cursor-pointer transition-colors h-fit'
-                                onClick={() => setSelectedTab(item)}
-                            >
-                                <span className='font-medium'>{item.label} </span>
-                                <span className='text-content text-sm'>({item.images.length})</span>
-
-                                {item.id === selectedTab.id && (
-                                    <motion.div
-                                        layoutId='underline'
-                                        className='absolute left-0  -bottom-1 h-[2px] rounded-full bg-primary w-full'
-                                        transition={{ type: 'spring', stiffness: 500, damping: 30 }}
-                                    />
-                                )}
-                            </motion.li>
-                        ))}
-                    </ul>
+                    <NavList
+                        media={media}
+                        selectedTab={selectedTab}
+                        setSelectedTab={setSelectedTab}
+                    />
                 </nav>
 
                 <div className='overflow-hidden relative w-full'>
@@ -76,29 +62,10 @@ export function MovieMedia(props: Props) {
                             exit={{ x: -10, opacity: 0 }}
                             transition={{ duration: 0.2 }}
                         >
-
-                            <ol className='flex py-5 gap-2 overflow-x-scroll'>
-                                {selectedTab.images.map((image: any, index: number) => (
-                                    <li
-                                        key={index}
-                                        className='block shrink-0 cursor-pointer'
-                                        onClick={() => handleOpen(index)}
-                                    >
-                                        <motion.div
-                                            initial={{ scale: 1 }}
-                                            whileHover={{ scale: 1.15, zIndex: 15 }}
-                                            style={{
-                                                backgroundImage: `url(${selectedTab.id === 0 ? getBackdropImage(image.file_path) : getPosterImage(image.file_path)})`,
-                                                width: selectedTab.id === 0 ? 375 : 175,
-                                                aspectRatio: selectedTab.id === 0 ? '16/9' : '2/3',
-                                            }}
-                                            className='relative bg-foreground-light rounded-xl bg-center bg-cover mx-auto z-10'
-                                        >
-                                            <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-xl transition-opacity'></div>
-                                        </motion.div>
-                                    </li>
-                                ))}
-                            </ol>
+                            <ImagesList
+                                selectedTab={selectedTab}
+                                handleOpen={handleOpen}
+                            />
                         </motion.div>
                     </AnimatePresence>
                 </div>
@@ -115,5 +82,76 @@ export function MovieMedia(props: Props) {
                 </AnimatePresence>
             </div>
         </section>
+    )
+}
+
+const NavList = ({
+    media,
+    selectedTab,
+    setSelectedTab,
+}: {
+    media: Media[],
+    selectedTab: any,
+    setSelectedTab: (item: any) => void,
+}) => {
+    return (
+        <ul className='flex gap-6 relative'>
+            {media.map((item) => (
+                <motion.li
+                    key={item.id}
+                    initial={false}
+                    whileHover={{
+                        color: item.id === selectedTab.id ? 'var(--foreground)' : 'var(--primary)',
+                        cursor: item.id === selectedTab.id ? 'default' : 'pointer'
+                    }}
+                    className='relative block cursor-pointer transition-colors h-fit'
+                    onClick={() => setSelectedTab(item)}
+                >
+                    <span className='font-medium'>{item.label} </span>
+                    <span className='text-content text-sm'>({item.images.length})</span>
+
+                    {item.id === selectedTab.id && (
+                        <motion.div
+                            layoutId='underline'
+                            className='absolute left-0  -bottom-1 h-[2px] rounded-full bg-primary w-full'
+                            transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                        />
+                    )}
+                </motion.li>
+            ))}
+        </ul>
+    )
+}
+
+const ImagesList = ({
+    selectedTab,
+    handleOpen,
+}: {
+    selectedTab: any,
+    handleOpen: (index: number) => void,
+}) => {
+    return (
+        <ol className='flex py-5 gap-2 overflow-x-scroll'>
+            {selectedTab.images.map((image: any, index: number) => (
+                <li
+                    key={index}
+                    className='block shrink-0 cursor-pointer'
+                    onClick={() => handleOpen(index)}
+                >
+                    <motion.div
+                        initial={{ scale: 1 }}
+                        whileHover={{ scale: 1.15, zIndex: 15 }}
+                        style={{
+                            backgroundImage: `url(${selectedTab.id === 0 ? getBackdropImage(image.file_path) : getPosterImage(image.file_path)})`,
+                            width: selectedTab.id === 0 ? 375 : 175,
+                            aspectRatio: selectedTab.id === 0 ? '16/9' : '2/3',
+                        }}
+                        className='relative bg-foreground-light rounded-xl bg-center bg-cover mx-auto z-10'
+                    >
+                        <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-xl transition-opacity'></div>
+                    </motion.div>
+                </li>
+            ))}
+        </ol>
     )
 }
