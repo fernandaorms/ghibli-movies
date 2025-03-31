17af531c190bcc473c44779d1a7087cce7bcd648
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-03-31 09:55:03 -0300

    movie-credits - refactoring

--- src/layout/movie-credits.tsx
+++ src/layout/movie-credits.tsx
@@ -6,11 +6,17 @@ type Props = {
     movieCredits: any,
 }
 
-export function MovieCredits(props: Props) {
-    const cast = props.movieCredits.cast ?? null;
-    const crew = props.movieCredits.crew ?? null;
+type Credits = {
+    id: number,
+    label: string,
+    info: any,
+}
 
-    const credits = [
+export function MovieCredits({ movieCredits }: Props) {
+    const cast = movieCredits.cast ?? null;
+    const crew = movieCredits.crew ?? null;
+
+    const credits: Credits[] = [
         { id: 0, label: 'Cast', info: cast },
         { id: 1, label: 'Crew', info: crew }
     ];
@@ -23,31 +29,11 @@ export function MovieCredits(props: Props) {
                 <nav className='flex gap-10 md:gap-16 items-baseline pb-2'>
                     <h2 className='font-medium text-xl md:text-2xl'>Credits</h2>
 
-                    <ul className='flex gap-6 relative'>
-                        {credits.map((item) => (
-                            <motion.li
-                                key={item.id}
-                                initial={false}
-                                whileHover={{
-                                    color: item.id === selectedTab.id ? 'var(--foreground)' : 'var(--primary)',
-                                    cursor: item.id === selectedTab.id ? 'default' : 'pointer'
-                                }}
-                                className='relative block cursor-pointer transition-colors h-fit'
-                                onClick={() => setSelectedTab(item)}
-                            >
-                                <span className='font-medium'>{item.label} </span>
-                                <span className='text-content text-sm'>({item.info.length})</span>
-
-                                {item.id === selectedTab.id && (
-                                    <motion.div
-                                        layoutId='underline'
-                                        className='absolute left-0  -bottom-1 h-[2px] rounded-full bg-primary w-full'
-                                        transition={{ type: 'spring', stiffness: 500, damping: 30 }}
-                                    />
-                                )}
-                            </motion.li>
-                        ))}
-                    </ul>
+                    <NavList
+                        credits={credits}
+                        selectedTab={selectedTab}
+                        setSelectedTab={setSelectedTab}
+                    />
                 </nav>
 
                 <AnimatePresence mode='wait'>
@@ -62,29 +48,76 @@ export function MovieCredits(props: Props) {
                         <div className='absolute top-0 left-0 h-full w-10 bg-linear-to-r from-background to-transparent z-10'></div>
                         <div className='absolute top-0 right-0 h-full w-10 bg-linear-to-l from-background to-transparent z-10'></div>
 
-                        <ol className='flex py-4 overflow-x-scroll'>
-                            {selectedTab.info.map((person: any, index: number) => (
-                                <li key={index} className='block !w-36 shrink-0'>
-                                    <motion.div
-                                        initial={{ scale: 1 }}
-                                        // whileHover={{ scale: 1.25, zIndex: 15 }}
-                                        style={{ backgroundImage: `url(${getProfileImage(person.profile_path)})` }}
-                                        className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-10'
-                                    >
-                                        {/* <div className='absolute top-0 left 0 h-full w-full opacity-0 md:opacity-35 bg-black hover:opacity-0 rounded-full transition-opacity'></div> */}
-                                    </motion.div>
-
-                                    <div className='text-center'>
-                                        <span className='block font-semibold mt-3'>{person.name}</span>
-                                        <span className='block text-xs font-light mb-1 text-content'>({person.original_name})</span>
-                                        <span className='block text-sm text-content'>{selectedTab.id === 0 ? person.character : person.job}</span>
-                                    </div>
-                                </li>
-                            ))}
-                        </ol>
+                        <CreditsList
+                            selectedTab={selectedTab}
+                        />
                     </motion.div>
                 </AnimatePresence>
             </div>
         </section>
+    )
+}
+
+const NavList = ({
+    credits,
+    selectedTab,
+    setSelectedTab,
+}: {
+    credits: Credits[],
+    selectedTab: any,
+    setSelectedTab: (item: any) => void,
+}) => {
+    return (
+        <ul className='flex gap-6 relative'>
+            {credits.map((item) => (
+                <motion.li
+                    key={item.id}
+                    initial={false}
+                    whileHover={{
+                        color: item.id === selectedTab.id ? 'var(--foreground)' : 'var(--primary)',
+                        cursor: item.id === selectedTab.id ? 'default' : 'pointer'
+                    }}
+                    className='relative block cursor-pointer transition-colors h-fit'
+                    onClick={() => setSelectedTab(item)}
+                >
+                    <span className='font-medium'>{item.label} </span>
+                    <span className='text-content text-sm'>({item.info.length})</span>
+
+                    {item.id === selectedTab.id && (
+                        <motion.div
+                            layoutId='underline'
+                            className='absolute left-0  -bottom-1 h-[2px] rounded-full bg-primary w-full'
+                            transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                        />
+                    )}
+                </motion.li>
+            ))}
+        </ul>
+    )
+}
+
+const CreditsList = ({
+    selectedTab
+}: {
+    selectedTab: any
+}) => {
+    return (
+        <ol className='flex py-4 overflow-x-scroll'>
+            {selectedTab.info.map((person: any, index: number) => (
+                <li key={index} className='block !w-36 shrink-0'>
+                    <motion.div
+                        initial={{ scale: 1 }}
+                        style={{ backgroundImage: `url(${getProfileImage(person.profile_path)})` }}
+                        className='relative bg-foreground-light h-24 w-24 rounded-full flex items-center justify-center bg-center bg-cover mx-auto z-10'
+                    />
+
+                    <div className='text-center'>
+                        <span className='block font-semibold mt-3'>{person.name}</span>
+                        <span className='block text-xs font-light mb-1 text-content'>({person.original_name})</span>
+                        <span className='block text-sm text-content'>{selectedTab.id === 0 ? person.character : person.job}</span>
+                    </div>
+                </li>
+            ))}
+        </ol>
     )
 }
