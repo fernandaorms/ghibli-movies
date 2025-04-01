c29e1a6b3fdd68f7cbec7afbb71d04225d31aefb
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2025-04-01 12:52:07 -0300

    home - about done

--- src/layout/home/about.tsx
+++ src/layout/home/about.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { motion, useAnimation, useInView } from 'motion/react';
+import { AnimationControls, motion, useAnimation, useInView } from 'motion/react';
 import { useEffect, useRef } from 'react';
 
 export function About() {
-    const ref = useRef(null);
+    const ref = useRef<HTMLDivElement | null>(null);
     const inView = useInView(ref, { once: true })
 
     const mainControls = useAnimation();
@@ -18,9 +18,9 @@ export function About() {
     return (
         <section
             id='about'
-            className='h-fit min-h-[50vh] max-h-[720px] py-12 lg:py-20 xl:py-28'
+            className='min-h-[70vh] py-12 lg:py-20 xl:py-28 flex items-center justify-center'
         >
-            <div className='wrapper'>
+            <div className='wrapper mb-6'>
                 <motion.div
                     ref={ref}
                     variants={{
@@ -37,12 +37,69 @@ export function About() {
                         <p className='text-content'>This project is a Studio Ghibli catalog where you can search for movies and learn more about them, including ratings, overviews, galleries and users reviews.</p>
                     </div>
 
-                    <div>
+                    <div className='flex flex-col lg:flex-row items-center lg:items-stretch justify-center gap-8 lg:gap-10 mt-6 xl:mt-10 '>
+                        <SingleStep
+                            number={1}
+                            title={'Search for a movie'}
+                            content={'Type the name of a Studio Ghibli movie in the search bar to find what you\'re looking for.'}
+                            delay={0.5}
+                            mainControls={mainControls}
+                        />
 
+                        <SingleStep
+                            number={2}
+                            title={'Click on Search'}
+                            content={'Hit the "Search" button to see all the results found!'}
+                            delay={1}
+                            mainControls={mainControls}
+                        />
+
+                        <SingleStep
+                            number={3}
+                            title={'Have fun'}
+                            content={'Enjoy browsing the full catalog and discovering more about these amazing movies!'}
+                            delay={1.5}
+                            mainControls={mainControls}
+                        />
                     </div>
                 </motion.div>
             </div>
-
         </section>
+    )
+}
+
+const SingleStep = ({
+    number,
+    title,
+    content,
+    delay,
+    mainControls,
+}: {
+    number: number
+    title: string,
+    content: string,
+    delay: number
+    mainControls: AnimationControls,
+}) => {
+    return (
+        <motion.div
+            className='w-60 text-center'
+            variants={{
+                hidden: { opacity: 0, x: -10 },
+                visible: { opacity: 1, x: 0 },
+            }}
+            initial='hidden'
+            animate={mainControls}
+            transition={{ duration: 0.5, delay: delay }}
+        >
+            <div className='h-16 lg:h-20 w-16 lg:w-20 flex items-center justify-center bg-primary-md rounded-full mx-auto border-4 border-white-20'>
+                <span className='font-semibold text-2xl lg:text-3xl text-white'>{number}</span>
+            </div>
+
+            <div className='mt-4'>
+                <span className='block mb-2 uppercase font-bold'>{title}</span>
+                <p className='block flex-1 text-sm text-content'>{content}</p>
+            </div>
+        </motion.div>
     )
 }
